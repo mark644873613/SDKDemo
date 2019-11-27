8fe7f55030b9bc2a0ffc866d5c0e3e4a51e88c84
guoxin <644873613@qq.com>
2019-11-27 14:50:00 +0800

    init

---
+++ DDDemoSDK/DDDemoSDK/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DDDemoSDK
+//
+//  Created by Apple on 2019/11/27.
+//  Copyright © 2019年 xin. All rights reserved.
+//
+
+import UIKit
+import DemoSDK
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let sdk = XXName()
+        sdk.xxp2()
+        sdk.xxp()
+    }
+
+
+}
+
---
+++ DDDemoSDK/DemoSDK.framework/Headers/DemoSDK.h
@@ -0,0 +1,19 @@
+//
+//  DemoSDK.h
+//  DemoSDK
+//
+//  Created by Apple on 2019/11/26.
+//  Copyright © 2019年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoSDK.
+FOUNDATION_EXPORT double DemoSDKVersionNumber;
+
+//! Project version string for DemoSDK.
+FOUNDATION_EXPORT const unsigned char DemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoSDK/PublicHeader.h>
+
+
